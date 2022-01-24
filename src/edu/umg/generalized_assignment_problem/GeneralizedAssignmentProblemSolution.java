6e9607e436e57479dc9e7ea565f46801319f508f
KKlejbor <krzysztof.klejbor@gmail.com>
2022-01-24 23:58:53 +0100

    Create GeneralizedAssignmentProblemSolution class

---
+++ src/edu/umg/generalized_assignment_problem/GeneralizedAssignmentProblemSolution.java
@@ -0,0 +1,18 @@
+package edu.umg.generalized_assignment_problem;
+
+public class GeneralizedAssignmentProblemSolution implements Solution{
+	@Override
+	public double getValue() {
+		return 0;
+	}
+
+	@Override
+	public Solution findSolutionFromNeighbourhood() {
+		return null;
+	}
+
+	@Override
+	public Solution newInstance() {
+		return null;
+	}
+}
